1a5c838a0a093f146ffad9bbb7679d3adcf65ad9
RaedSbenaty <raeed.sbenaty.963@gmail.com>
2021-05-07 10:17:36 +0300

    initial data for teacher, account and info + sign up for teacher + initial auth

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -5,7 +5,4 @@ const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, p
     dialect: 'mssql'
 });
 
-sequelize.authenticate().then(() => console.log('Connection has been established successfully.'))
-    .catch((error) => console.error('Unable to connect to the database:', error))
-
 module.exports = sequelize
--- src/models/teacher.js
+++ src/models/teacher.js
@@ -1,18 +1,24 @@
 var {DataTypes, Model} = require('sequelize')
 var sequelize = require('../db/sequelize')
+var Personal_Info = require('./personal-info')
+var Account = require('./account')
 
 class Teacher extends Model {
-
 }
 
 Teacher.init({
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },lastName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
+    certification: {type: DataTypes.STRING, allowNull: false},
+    certificationIssuer: {type: DataTypes.STRING},
+    certificationDate: {type: DataTypes.DATE},
 }, {sequelize})
+
+
+
+Account.hasOne(Teacher)
+Teacher.belongsTo(Account, {foreignKey: {allowNull: false}})
+
+Personal_Info.hasOne(Teacher)
+Teacher.belongsTo(Personal_Info, {foreignKey: {allowNull: false}})
+
 
 module.exports = Teacher

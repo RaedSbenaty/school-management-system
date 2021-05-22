92c794f6c00a7902a7c9cc142672c4da20b01a1f
Abd Al Rahman Shebani <shebaniabdalrahman@gmail.com>
2021-05-23 01:46:14 +0300

    Senior AS7 Was Here

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ var schoolRouter = require('./routers/school')
 app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'POST')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
     next()
 })
 

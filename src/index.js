9d65b38de866e6a6a0246b503fc43524f4c84149
RaedSbenaty <raeed.sbenaty.963@gmail.com>
2021-08-01 17:11:09 +0300

    get teachers for a student in a semester

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const AnnouncementType = require('./models/announcement/announcementType')
 
 const runServer = async () => {
     await sequelize.authenticate()
-    await sequelize.sync({force: true})
+//    await sequelize.sync({force: true})
     await sequelize.sync()
     await Class.bulkCreate(Class.defaultClasses)
     await Category.bulkCreate(Category.defaultCategories)
--- src/routers/student.js
+++ src/routers/student.js
@@ -5,12 +5,15 @@ const auth = require('../middlewares/auth')
 const belongsTo = require('../middlewares/studentBelongsToSchool')
 
 const Student = require('../models/student/student')
+const Teacher = require('../models/teacher/teacher')
 const Announcement = require('../models/announcement/announcement')
 const Absence = require('../models/session/absence')
 const Day = require('../models/session/day')
 const SchoolClass = require('../models/class/schoolClass')
 const SubjectInSemester = require('../models/subject/subjectInSemester')
 const InLocoParentis = require('../models/inLocoParentis')
+
+
 //example
 /*
 {
@@ -178,6 +181,49 @@ router.get('/students/:studentId/:siteName/:startYear-:endYear/examSchedule', au
             if (!classroomSchedule.length)
                 return res.status(404).send('No exam schedule was found for the specified classroom in the specified year.')
             res.status(201).send(classroomSchedule[0].classrooms[0].examSchedules)
+        } catch (e) {
+            console.log(e)
+            res.status(500).send(e.message)
+        }
+    })
+
+
+// get teachers for a student in a semester
+// /students/1/alhbd/2020-2021/semesters/1/teachers
+router.get('/students/:studentId/:siteName/:startYear-:endYear/semesters/:semesterNumber/teachers',
+    auth(['Student']), belongsTo, async (req, res) => {
+        try {
+            const teachers = await Teacher.findAll({
+                attributes: ['id'], include: [
+                    {association: 'personalInfo', attributes: ['firstName', 'lastName']},
+                    {association: 'account', attributes: ['email', 'phoneNumber']},
+                    {
+                        association: 'teacherInSchools', attributes: ['id'], required: true, include: {
+                            association: 'teacherInYears', attributes: ['id'], required: true, include: {
+                                association: 'teacherInClasses', attributes: ['id'], required: true, include: {
+                                    association: 'sessions', attributes: ['id'],
+                                    where: {classroomId: req.studentInClass.classroomId},
+                                    include: {
+                                        association: 'subjectInSemester', attributes: ['id'],
+                                        where: {semester: req.params.semesterNumber},
+                                        include: {association: 'subjectInYear', attributes: ['name']}
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            })
+
+            for (const teacher of teachers) {
+                const sessions = teacher.teacherInSchools[0].teacherInYears[0].teacherInClasses[0].sessions
+                delete teacher.dataValues.teacherInSchools
+                const subjectsSet = new Set()
+                sessions.forEach(session => subjectsSet.add(session.subjectInSemester.subjectInYear.name))
+                teacher.dataValues.subjects = Array.from(subjectsSet)
+            }
+
+            res.send(teachers)
         } catch (e) {
             console.log(e)
             res.status(500).send(e.message)
